f08ba79b88cb51ca1bf7a0b7ff191e5d07d4b0ab
Oliver1334 <oliver.taylor95@outlook.com>
2026-03-08 00:53:10 +0000

    Dummy blog post page added linked to dev blog card

--- src/blogPosts/posts.js
+++ src/blogPosts/posts.js
@@ -1,9 +1,16 @@
+import { BuildingThisSite } from "./BuildingThisSite"
+
+
+
+
+
+
 export const blogPosts = [
     {
         slug: "building-this-site",
         title: "Building This Site",
         date: "2026-03-07",
         excerpt: "This is my first blog post, today i will be talking about how i made this portfolio website with react and tailwind...",
-        component: null //filled in once page component is created
+        component: BuildingThisSite
     }
 ]
