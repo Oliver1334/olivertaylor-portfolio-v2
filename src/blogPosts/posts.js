af86a3ade632be76e62a296bde999156e0df0ffe
Oliver1334 <oliver.taylor95@outlook.com>
2026-03-15 02:56:34 +0000

    Devblog first article redraft, image updates

--- src/blogPosts/posts.js
+++ src/blogPosts/posts.js
@@ -2,10 +2,10 @@ import { NCGamesToNDG } from "./NCGamesToNDG"
 
 export const blogPosts = [
     {
-        slug: "building-this-site",
+        slug: "NCGames-to-NDG",
         title: "From NC Games to No Dice Games",
-        date: "March 7, 2026",
-        excerpt: "The first rendition of my board games review website 'NC Games' looked like a frontend built by a backend developer. After getting my backend RESTful API up and running with a list of endpoints to check through developed with thorough testing and getting my head round SQL left joins and all that jazz, the prospect of designing a polished frontend for a user did not excite me...",
+        date: "March 16, 2026",
+        excerpt: "The first rendition of my board games review website 'NC Games' looked like a frontend built by a backend developer. After getting my backend RESTful API showing a list of endpoints and getting my head around SQL left joins and all that jazz, the prospect of designing a polished, responsive frontend for a user did not excite me...",
         component: NCGamesToNDG
     }
 ]
